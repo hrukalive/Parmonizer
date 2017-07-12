8c344f242494d72d141d2d19ea964ae0c5fc3fee
hrukalive <bowen.parallife@gmail.com>
2017-07-12 12:50:58 -0400

    Prepare for parsing.

--- src/com/parser/IParser.java
+++ src/com/parser/IParser.java
@@ -4,12 +4,12 @@ package com.parser;
  * Created by NyLP on 7/12/17.
  */
 
-interface ParserExp<T>
+interface IParserExp<T>
 {
     T eval();
 }
 
 public interface IParser<T>
 {
-    ParserExp<T> parse(String expr);
+    IParserExp<T> parse(String expr);
 }
